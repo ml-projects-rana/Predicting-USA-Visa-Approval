05ae255ccdb5aadade2ca2385bdfa1badbce0508
Dipanshu Rana <dipanshurana04@gmail.com>
2024-05-27 14:43:18 +0100

    updation 1

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.0.0",
     author="Rana",
     author_email="ml.coding.rana@gmail.com",
-    packages=find_packages()
+    packages=find_packages()  #finding local packages inside us_visa . Note: us_visa is also a local package only..
 )
