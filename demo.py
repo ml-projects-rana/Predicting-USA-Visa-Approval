1615fe04023522790b3460ba225d6b5ca7a3cff8
Dipanshu Rana <dipanshurana04@gmail.com>
2024-05-29 21:37:09 +0100

    update 3

--- demo.py
+++ demo.py
@@ -2,9 +2,19 @@ from us_visa.logger import logging
 from us_visa.exception import USvisaException
 import sys
 
-try:
-    a=1/"10"
-except Exception as e:
-    logging.exception(e)
-    raise USvisaException(e,sys) from e
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+# try:
+#     a=1/"10"
+# except Exception as e:
+#     logging.exception(e)
+#     raise USvisaException(e,sys) from e
+
+# from us_visa.constants import *
+# print(COLLECTION_NAME)
+# print(DATABASE_NAME)
+
+pipline = TrainPipeline()
+
+pipline.run_pipeline()
 
